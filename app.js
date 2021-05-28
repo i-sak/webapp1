4c78a61aa80827754fbf864ade4c35650b85e171
kimisak <isak7263@gmail.com>
2021-05-28 23:06:36 +0900

    index.html file service start!

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '10.138.0.2';
+//hostname = '127.0.0.1';
+
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  
+  fs.readFile("./page/index.html", "UTF-8",
+    function(err,data) {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'text/html'); //'text/plain'
+      res.write(data);  // 2021-05-28
+      res.end();
+      //res.end('Hello World');
+    });
+  
 });
 
 server.listen(port, hostname, () => {
