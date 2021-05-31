7304f1f849154d2890e5d438e9528b5781b4daff
kimisak <isak7263@gmail.com>
2021-05-31 23:37:55 +0900

    node + express

--- server/app.js
+++ server/app.js
@@ -1,14 +1,35 @@
-const http = require('http');
-const fs = require('fs');
+// index.js
+// npm install express
 
-hostname = '0.0.0.0';
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
 
 const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+/*
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const port = 3000;
+hostname = '0.0.0.0';
 
 const server = http.createServer((req, res) => {
   
-  fs.readFile("./page/index.html", "UTF-8",
+  var staticFilePath = path.join(__dirname, "/public/");
+
+  
+
+  fs.readFile(staticFilePath+"/page/test.js", "UTF-8",
     function(err,data) {
+      
+      var tem = data;
+      console.log(tem);
       res.statusCode = 200;
       res.setHeader('Content-Type', 'text/html'); //'text/plain'
       res.write(data);  // 2021-05-28
@@ -22,3 +43,4 @@ server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
 
+*/
